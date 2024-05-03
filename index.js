5301fa0aff7689ea654db9e53906ef356f1ccaea
jessinabrandao <jessinafreitas.brandao@gmail.com>
2024-05-03 13:20:25 -0300

    primeira etapa do desafio

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-dad
+let nome = "Jéssina"
+let xp = 24
+const mensagem = "você está na liga"
+
+if (xp <= 1000){
+    console.log( nome + " " + mensagem + " "+ "Ferro")
+}
+
+else if (xp >= 1001 && xp <= 2000){
+    console.log(nome + " " + mensagem+ " " + "Bronze")
+}
+
+else if (xp >= 2001 && xp <= 5000){
+    console.log(nome +" "+ mensagem+ " " + "Prata Ouro")
+}
+else if (xp >= 5001 && xp <=8000){
+    console.log(nome + " " + mensagem + " " + "Platina Diamante")
+}
+else if (xp >= 9001 && xp <= 9000){
+    console.log(nome + " " + mensagem + " " + "Ascendente")
+}
+else if (xp >= 9001 && xp <= 10000){
+    console.log(nome + " " + mensagem + " " + "Imortal")
+}
+else if (xp >= 10001){
+    console.log(nome + " " + mensagem + " " + "Radiante")
+}
